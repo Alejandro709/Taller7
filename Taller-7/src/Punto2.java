933c8d8576ae537124bc734890d70d3e7569f162
Juan Andrés <juan.hurtado10@usc.edu.co>
2023-03-31 16:17:01 -0500

    Subo taller 7

---
+++ Taller-7/src/Punto2.java
@@ -0,0 +1,20 @@
+
+public class Punto2 {
+	public static void main(String[]args) {
+	
+	String str= "hello world";
+
+
+	for(int i = 0; i < 4; i++)
+	{
+	System.out.println(str);
+
+	
+	str+= "!";
+	}
+
+	
+	
+
+}
+}
---
+++ Taller-7/src/Punto4/Sintaxix.java
@@ -0,0 +1,18 @@
+package Punto4;
+
+public class Sintaxix {
+	public static void main(String[]args) {
+		
+	String str= "hello world";
+
+
+	for(int i = 0; i < 4; i++);
+	{
+	System.out.println(str);
+
+	
+	str+= "!";
+	}
+
+}
+}
